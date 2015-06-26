170ee0099649353fb2752469a0283fa1dcd705ba
Bob <516718650@qq.com>
2015-06-26 20:15:13 +0800

    完成遍历市县三级列表功能

--- src/model/CoolWeatherDB.java
+++ src/model/CoolWeatherDB.java
@@ -87,27 +87,28 @@ public class CoolWeatherDB {
 		return list;
 	}
 
-	public void saveCountry(Country country) {
-		if (country != null) {
+	public void saveCounty(County county) {
+		if (county != null) {
 			ContentValues values = new ContentValues();
-			values.put("country_name", country.getCountryName());
-			values.put("country_code", country.getCountryCode());
-			values.put("city_id", country.getCityId());
-			db.insert("Country", null, values);
+			values.put("county_name", county.getCountyName());
+			values.put("county_code", county.getCountyCode());
+			values.put("city_id", county.getCityId());
+			db.insert("county", null, values);
 		}
 	}
 	
-	public List<Country> loadCountry(int cityId){
+	public List<County> loadCounty(int cityId){
 		
-		List<Country> list=new ArrayList<Country>();
-		Cursor cursor=db.query("Country", null, "city_id=?", new String[]{String.valueOf(cityId)}, null, null,null);
+		List<County> list=new ArrayList<County>();
+		Cursor cursor=db.query("county", null, "city_id=?", new String[]{String.valueOf(cityId)}, null, null,null);
 		if(cursor.moveToFirst()){
 			do{
-				Country country=new Country();
-				country.setId(cursor.getInt(cursor.getColumnIndex("id")));
-				country.setCountryName(cursor.getString(cursor.getColumnIndex("country_name")));
-				country.setCityId(cityId);
-				list.add(country);
+				County county=new County();
+				county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+				county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+				county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+				county.setCityId(cityId);
+				list.add(county);
 			}while(cursor.moveToNext());
 		}
 		return list;
